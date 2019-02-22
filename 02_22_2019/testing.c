d380a0047510249261c4c8517836e9c8da018ba3
Jose Antonio Maria de Rezende Silva <30808376+tonykingnz@users.noreply.github.com>
2019-02-22 11:43:00 -0300

    Add files via upload

---
+++ 02_22_2019/testing.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+  int nota1, nota2, nota3, nota4, mediaFinal;
+  printf("Escreva suas 4 notas separadas por espaco e em numeros inteiros sem virgula, apenas 1 a 10.\n");
+  scanf("%i %i %i %i\n", &nota1, &nota2, &nota3, &nota4);
+  mediaFinal = (nota1 + nota2 + nota3 + nota4)/4
+  printf("Sua media final eh %i\n",mediaFinal );
+  return 0;
+}
