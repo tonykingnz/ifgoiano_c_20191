0bf1bc64b670eed20e9424498e4fcc79655b0796
Jose Antonio Maria de Rezende Silva <30808376+tonykingnz@users.noreply.github.com>
2019-02-22 11:47:15 -0300

    Add files via upload

---
+++ 02_22_2019/evenOrOdd.c
@@ -0,0 +1,22 @@
+//by tonyKingNZ©
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int num = 0;
+    printf("Escreva um numero(Apenas numeros inteiros):\n");
+	scanf ("%d",&num);
+    if (num % 2 == 0) {
+
+    	printf("Este numero eh par.\n");
+
+}
+
+    else {
+		printf("Este numero eh impar.!\n");
+
+  }
+	  return 0;
+}
