e2f8a9e2bb722ce44d6bef412745ef140ec1eeae
Jose Antonio Maria de Rezende Silva <30808376+tonykingnz@users.noreply.github.com>
2019-06-15 09:47:28 -0300

    Add files via upload

---
+++ lista_De_Exercicios_Moodle_Para_06_15/questao6Perfil.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+	int dia, mes, ano, numeroPerfil;
+	
+	printf("Escreva o dia do seu nascimento. (Apenas o dia, no modelo dd)\n");
+	scanf("%i", &dia);
+	
+	printf("Escreva o mes do seu nascimento. (Apenas o mes, no modelo mm)\n");
+	scanf("%i", &mes);
+	
+	printf("Escreva o ano do seu nascimento. (Apenas o ano no modelo aaaa)\n");
+	scanf("%i", &ano);
+	
+	numeroPerfil= (((dia*100) + mes) + ano) 
+	
+	
+	
+	
+	
+	return 0;
+}
+
