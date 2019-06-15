12988e95121d278d3566eaf4f972015fce852655
Jose Antonio Maria de Rezende Silva <30808376+tonykingnz@users.noreply.github.com>
2019-06-15 11:43:20 -0300

    Add files via upload

--- lista_De_Exercicios_Moodle_Para_06_15/questao6Perfil.c
+++ lista_De_Exercicios_Moodle_Para_06_15/questao6Perfil.c
@@ -3,22 +3,30 @@
 #include <math.h>
 
 int main() {
-	int dia, mes, ano, numeroPerfil;
+	int dia, mes, ano, passo1A3, passo4, passo5, numeroPerfil;
 	
 	printf("Escreva o dia do seu nascimento. (Apenas o dia, no modelo dd)\n");
-	scanf("%i", &dia);
+	scanf("%d", &dia);
 	
 	printf("Escreva o mes do seu nascimento. (Apenas o mes, no modelo mm)\n");
-	scanf("%i", &mes);
+	scanf("%d", &mes);
 	
 	printf("Escreva o ano do seu nascimento. (Apenas o ano no modelo aaaa)\n");
-	scanf("%i", &ano);
+	scanf("%d", &ano);
 	
-	numeroPerfil= (((dia*100) + mes) + ano) 
+	passo1A3 = (((dia*100) + mes) + ano);
+	passo4 = passo1A3/100;
+	passo5 = passo1A3%100;
+	numeroPerfil = ((passo4 + passo5)%5);
 	
-	
-	
-	
+	switch (numeroPerfil){
+		case 0: printf("\nVoce eh Timido\n"); break;
+		case 1: printf("\nVoce eh Sonhador\n"); break;	
+		case 2: printf("\nVoce eh Paquerador\n"); break;
+		case 3: printf("\nVoce eh Atraente\n"); break;
+		case 4: printf("\nVoce eh Irresistivel\n"); break;
+		defaut: printf("\nOuve um erro inesperado.\nTente novamente.\nInsira seus dados novamente, como orientado\n");
+	}
 	
 	return 0;
 }
